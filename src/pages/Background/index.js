1a5716d647e3d3f54bd79052230b6b39b1045e1a
Liang Liang <lilysway@hotmail.com>
2020-05-01 09:26:08 -0400

    figuring out how to get cookie stored

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -4,3 +4,32 @@ import '../../assets/img/logo128.png';
 
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({ color: '#3aa757' }, function () {
+    console.log('The color is green.');
+  });
+});
+
+console.log('chrome.storage', chrome.storage);
+console.log('session', document.cookie);
+
+var getCookie = function (cname) {
+  console.log('running');
+  console.log('document.cookie', document.cookie);
+  var name = cname + '=';
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+};
+
+console.log('cookie', getCookie('user'));
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,3 +4,8 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+// let images = document.getElementsByTagName('img')
+// for(image of images){
+//   image.style[]
+// }
